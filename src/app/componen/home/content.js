e5710fd3ace4134ecd76da7a800dc5262a6b2fab
anwar <agus.anwar072.@gmail.com>
2024-12-06 21:35:02 +0700

    integrasi API Plublic

--- src/app/componen/home/content.js
+++ src/app/componen/home/content.js
@@ -1,8 +1,52 @@
+"use client";
+
 import React from "react";
+import axios from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const content = () => {
-  return <div className="bg-gray-500 mt-6"></div>;
+  const [dataPosts, setDataPosts] = useState([]);
+
+  const url = "https://jsonplaceholder.typicode.com";
+
+  const getDataPosts = async () => {
+    try {
+      const response = await fetch(`${url}/posts`, {
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .catch((error) => console.log("eror"));
+
+      // const response = await axios.get(`${url}/posts`);
+
+      const result = response.data.reverse();
+
+      setDataPosts(result);
+    } catch (error) {
+      alert("Internal Server Error");
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getDataPosts();
+  }, []);
+
+  return (
+    <div className="bg-gray-500 mt-6">
+      {dataPosts.length && (
+        <div className="grid grid-cols-4 gap-4">
+          {dataPosts.map((item, index) => (
+            <div key={index} className="border border-2 border-black">
+              <p>{item.title}</p>
+              <p>{item.body}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default content;
--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Link href="/About">
         <p>Go To Abouut</p>
       </Link>
+      <Content />
     </div>
   );
 }
